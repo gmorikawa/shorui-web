06ce8348e2a0709ebd0a9d34e73b33b236c626f9
gmorikawa <morikawa.gabriel@outlook.jp>
2026-08-11 18:05:52 +0900

    chore: project clean-up

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainContainer } from '../components/containers/main-container/main-container';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MainContainer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/components/containers/main-container/main-container.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "sh-main-container",
+    templateUrl: "./main-container.html",
+    styleUrls: ["./main-container.scss"],
+})
+export class MainContainer {}
