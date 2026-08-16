143c24aef2d67446ca7f429e227b86fbe7afd96a
gmorikawa <morikawa.gabriel@outlook.jp>
2026-08-16 23:19:58 +0900

    feat: card and stack custom components

---
+++ src/components/card/card.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "sh-card",
+  templateUrl: "./card.html",
+  styleUrls: ["./card.scss"],
+})
+export class Card { }
---
+++ src/components/stack/stack.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "sh-stack",
+  templateUrl: "./stack.html",
+  styleUrls: ["./stack.scss"],
+})
+export class Stack { }
