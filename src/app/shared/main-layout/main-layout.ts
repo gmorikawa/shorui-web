c6dd3c2636b844062cf8de68265982d5a0bf3bc3
gmorikawa <morikawa.gabriel@outlook.jp>
2026-08-23 17:01:04 +0900

    feat: added navigation links to menu buttons

--- src/app/shared/main-layout/main-layout.ts
+++ src/app/shared/main-layout/main-layout.ts
@@ -1,6 +1,6 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { ShortcutButton } from '@components/buttons/shortcut-button/shortcut-button';
 import { MainContainer } from '@components/containers/main-container/main-container';
 import { WindowContainer } from '@components/containers/window-container/window-container';
@@ -17,4 +17,10 @@ import { WindowContainer } from '@components/containers/window-container/window-
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
-export class MainLayout { }
+export class MainLayout {
+  private readonly router = inject(Router);
+
+  protected handleNavigation(path: string): void {
+    this.router.navigate([path]);
+  }
+}
